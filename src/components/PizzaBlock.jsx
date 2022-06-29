25cc1862c6ec5e573a278492f1a3750d45e19aef
Anastasia951 <n.ivanova504@gmail.com>
2022-06-30 00:56:38 +0300

    adaptive

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,78 +1,83 @@
 import React, { useState } from 'react'
-import PropTypes from 'prop-types'
 import AddToCart from './AddToCart'
 
-function PizzaBlock({pizza, onClickAddPizza, cartCount}) {
+function PizzaBlock({ pizza, onClickAddPizza, cartCount }) {
   const types = ['тонкое', 'традиционное']
   const dimension = [26, 30, 40]
 
   const [activeType, setActiveType] = useState(0)
   const [activeSize, setActiveSize] = useState(0)
-  
-  const selectItem = (index) => {
+
+  const selectItem = index => {
     setActiveType(index)
   }
-  const selectSize = (index) => {
+  const selectSize = index => {
     setActiveSize(index)
   }
-  const onAddPizza = ({id, name, imageUrl, price}) => {
+  const onAddPizza = ({ id, name, imageUrl, price }) => {
     const pizza = {
-      id, name, imageUrl, price, size: dimension[activeSize], type: types[activeType]
+      id,
+      name,
+      imageUrl,
+      price,
+      size: dimension[activeSize],
+      type: types[activeType],
     }
 
     onClickAddPizza(pizza)
   }
   return (
-    <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src={pizza.imageUrl}
-        alt={pizza.name}
-      />
-      <h4 className="pizza-block__title">{pizza.name}</h4>
-      <div className="pizza-block__selector">
-        <ul>
-          {
-            pizza.types.map((type, index) => <li key={pizza.id + index} onClick={() => selectItem(type)}
-            className={pizza.types.includes(type) ? activeType === index ? 'active' : '' : 'disabled'}
-            >{types[index]}</li>
-            )
-          }
-        </ul>
-        <ul>
-        {
-            pizza.sizes.map((size, index) => <li key={pizza.id + size} onClick={() => selectSize(index)}
-            className={pizza.sizes.includes(size) ? index === activeSize ? 'active' : '' : 'disabled'}
-            >{dimension[index] + ` см`}</li>
-            )
-          }
-        </ul>
+    <div className='pizza-block'>
+      <div className='pizza-block__image'>
+        <img src={pizza.imageUrl} alt={pizza.name} />
       </div>
-      <div className="pizza-block__bottom">
-        <div className="pizza-block__price">от {pizza.price} ₽</div>
-        <AddToCart pizza={pizza} dispatch={onAddPizza} cartCount={cartCount}/>
+      <div className='pizza-info'>
+        <h4 className='pizza-block__title'>{pizza.name}</h4>
+        <div className='pizza-block__selector'>
+          <ul>
+            {pizza.types.map((type, index) => (
+              <li
+                key={pizza.id + index}
+                onClick={() => selectItem(type)}
+                className={
+                  pizza.types.includes(type)
+                    ? activeType === index
+                      ? 'active'
+                      : ''
+                    : 'disabled'
+                }>
+                {types[index]}
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {pizza.sizes.map((size, index) => (
+              <li
+                key={pizza.id + size}
+                onClick={() => selectSize(index)}
+                className={
+                  pizza.sizes.includes(size)
+                    ? index === activeSize
+                      ? 'active'
+                      : ''
+                    : 'disabled'
+                }>
+                {dimension[index] + ` см`}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className='pizza-block__bottom'>
+          <div className='pizza-block__price'>от {pizza.price} ₽</div>
+          <AddToCart
+            pizza={pizza}
+            dispatch={onAddPizza}
+            cartCount={cartCount}
+          />
+        </div>
       </div>
     </div>
   )
 }
 
-// PizzaBlock.propTypes = {
-//   pizza: {
-//     name: PropTypes.string.isRequired,
-//     imageUrl: PropTypes.string.isRequired,
-//     sizes: PropTypes.arrayOf([PropTypes.number]),
-//     types: PropTypes.arrayOf([PropTypes.string]),
-//     price: PropTypes.number.isRequired,
-//   }
-// }
-
-// PizzaBlock.defaultProps = {
-//   pizza: {
-//     types: [],
-//     sizes: [],
-//     name: 'Название',
-//     imageUrl: '',
-//     price: 0
-//   }
-// }
 export default PizzaBlock
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { CartPizzaBlock } from '../components'
 import { EmptyCart } from '../components/'
 import {
@@ -135,26 +136,11 @@ export default function Cart() {
                 </span>
               </div>
               <div className='cart__bottom-buttons'>
-                <a
-                  href='/'
+                <Link
+                  to={'/'}
                   className='button button--outline button--add go-back-btn'>
-                  <svg
-                    width='8'
-                    height='14'
-                    viewBox='0 0 8 14'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'>
-                    <path
-                      d='M7 13L1 6.93015L6.86175 1'
-                      stroke='#D3D3D3'
-                      strokeWidth='1.5'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                    />
-                  </svg>
-
                   <span>Вернуться назад</span>
-                </a>
+                </Link>
                 <div className='button pay-btn'>
                   <span>Оплатить сейчас</span>
                 </div>
